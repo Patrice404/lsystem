4a3ebf2bdbb73a71b35637746df879640609f8b0
Patrice404 <patricecotcho@gmail.com>
2025-11-29 22:19:12 +0100

    Ajout de Règles.md

---
+++ Règles.md
@@ -0,0 +1,11 @@
+# Des regles à essayer sur l'interface graphique
+    Crystal : axiom = F+F+F+F Règles : F:FF+F++F+F:100 angle = 90
+    Peano Curve : Axiom : X Règles : X:XFYFX+F+YFXFY-F-XFYFX and Y:YFXFY-F-XFYFX+F+YFXFY:100 angle = 90
+    Quadratic Snowflake : Axiom : F Règles : F:F-F+F+F-F:100 angle = 90
+    Variation by Hasan Hosam : axiom = X+X+X+X+X+X+X+X Règles : X:X+YF++YF-FX--FXFX-YF+X:100 and Y:-FX+YFYF++YF+FX--FX-YF:100 angle = 45
+    Board : axiom = F+F+F+F Règles : F:FF+F+F+F+FF angle = 90
+    Sierpinski Arrowhead : axiom = YF Règles : X:YF+XF+Y:100 and Y:XF-YF-X:100 angle = 60
+    Von Koch Snowflake : axiom = F++F++F Règles : F:F-F++F-F:100 angle = 60
+    Rings : axiom = F+F+F+F Règles : F:FF+F+F+F+F+F-F:100 angle = 90
+    Kolam : axiom = (-D--D) Règles : A:F++FFFF--F--FFFF++F++FFFF--F:100 and B:F--FFFF++F++FFFF--F--FFFF++F:100 and C:BFA--BFA:100 and D:CFC--CFC:100 angle = 45
+    Mango leaf : axiom = Y---Y Règles : X:{F-F}{F-F}--[--X]{F-F}{F-F}--{F-F}{F-F}--:100 and Y:f-F+X+F-fY:100 angle = 60
--- src/controler/AppController.java
+++ src/controler/AppController.java
@@ -244,22 +244,7 @@ public class AppController implements ActionListener {
 
         }
 
-        /*
-            Des regles à essayer sur l'interface graphique
-            Crystal : axiom = F+F+F+F Règles : F:FF+F++F+F:100 angle = 90
-            Peano Curve : Axiom : X Règles : X:XFYFX+F+YFXFY-F-XFYFX and Y:YFXFY-F-XFYFX+F+YFXFY:100 angle = 90
-            Quadratic Snowflake : Axiom : F Règles : F:F-F+F+F-F:100 angle = 90
-            Variation by Hasan Hosam : axiom = X+X+X+X+X+X+X+X Règles : X:X+YF++YF-FX--FXFX-YF+X:100 and Y:-FX+YFYF++YF+FX--FX-YF:100 angle = 45
-            Board : axiom = F+F+F+F Règles : F:FF+F+F+F+FF angle = 90
-            Sierpinski Arrowhead : axiom = YF Règles : X:YF+XF+Y:100 and Y:XF-YF-X:100 angle = 60
-            Von Koch Snowflake : axiom = F++F++F Règles : F:F-F++F-F:100 angle = 60
-            Rings : axiom = F+F+F+F Règles : F:FF+F+F+F+F+F-F:100 angle = 90
-        */
-        // F → FX[FX[+XF]] and X → FF[+XZ++X-F[+ZX]][-X++F-X] and Z → [+F-X-F][++ZX]
-        // Axiom => X Regle => F → FF and X → F+[-F-XF-X][+FF][--XF[+X]][++F-X] Angle =>
-        // 25° Itteration => 7
-
-    }
+ }
 
     /*
      * Gestion dynamique de la création et la suppression de nouvelle règle
